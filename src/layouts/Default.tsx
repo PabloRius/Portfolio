0b921361e484731dd1870ebb96fbef02fcdccc51
PabloRius <pablogrius@gmail.com>
2024-04-10 22:13:18 +0200

    Changed octokit lib for axios

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, RouteObject, Routes } from "react-router-dom";
-import { Octokit } from "octokit"
+import Axios from "axios"
 
 import { routes as def_routes } from "../routes/routes";
 
@@ -10,18 +10,12 @@ export const Default = () => {
     const [drawer, setDrawer] = useState(false)
     const [routes, setRoutes] = useState<RouteObject[]>(def_routes())
     useEffect(()=>{
-        const ok:Octokit = new Octokit({
-            auth: ""
-        })
 
-        ok.request('GET /users/PabloRius/repos', {
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        }).then((response)=>{
-            if(response?.status === 200){
-                console.log(response.data)
-                setRoutes(def_routes(response.data))
+        Axios.get("https://api.github.com/users/PabloRius/repos").then((response:any)=>{
+            if(response){
+                if(response?.status === 200){
+                    setRoutes(def_routes(response.data))
+                }
             }
         })
     },[])
