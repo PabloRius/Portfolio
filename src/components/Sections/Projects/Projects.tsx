152c810198cf5b0527ca312eb4f92d565472b07f
PabloRius <pablogrius@gmail.com>
2024-04-10 21:54:02 +0200

    No warnings, build safe

--- src/components/Sections/Projects/Projects.tsx
+++ src/components/Sections/Projects/Projects.tsx
@@ -20,6 +20,7 @@ export const Projects = ({projects}:ProjectsProps) => {
                         </div>
                     )
                 }
+                return <></>
             })}
         </div>
     )
