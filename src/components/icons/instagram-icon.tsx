422c54464fe57ee9c0d9bcc10ee7bc912fadfecd
PabloRius <pablogrius@gmail.com>
2025-05-15 19:47:59 +0100

    Bigger nav links

--- src/components/icons/instagram-icon.tsx
+++ src/components/icons/instagram-icon.tsx
@@ -2,8 +2,6 @@ export default function GithubIcon({ className }: { className?: string }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
